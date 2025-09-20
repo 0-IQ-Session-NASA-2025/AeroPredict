06ad0023011829fdecc4db9634bb2a06ea53d509
Arif Shahriar <arifshahriar028@gmail.com>
2025-09-20 16:23:19 +0600

    chore: update package.json

--- backend-node/src/index.ts
+++ backend-node/src/index.ts
@@ -9,7 +9,7 @@ import { Logger } from './utils/logger';
 
 import apiRoutes from './routes';
 
-DbConnector.connect({ host: process.env.DB_URL as string });
+// DbConnector.connect({ host: process.env.DB_URL as string });
 
 const app = express();
 const PORT = process.env.PORT;
