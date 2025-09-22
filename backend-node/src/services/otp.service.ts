0f4f4fd462572553128f15e0947fbb9525496808
Arif Shahriar <arifshahriar028@gmail.com>
2025-09-22 11:52:07 +0600

    feat: add mail sender api

--- backend-node/src/services/otp.service.ts
+++ backend-node/src/services/otp.service.ts
@@ -2,6 +2,8 @@ import mailSender from '../utils/mail-sender';
 import { setCache } from '../lib/redis';
 import { genHash } from '../utils/crypto';
 import validator from 'validator';
+import { apiFetch } from '../utils/fetch';
+import { sendOtp } from '../api/api';
 
 export const otpGen = (): string => {
     return Math.floor(100000 + Math.random() * 900000).toString();
@@ -13,9 +15,13 @@ export const otpSender = async (email: string): Promise<void> => {
     if (!validator.isEmail(email)) {
         throw new Error("Invalid Email");
     }
-    mailSender(email, otp);
-    console.log("=========>", otp)
-    await setCache(genHash(email), otp, 120);
+    // mailSender(email, otp);
+
+    const result = await apiFetch(sendOtp, 'POST', {email, otp});
+    if(result.success){
+        console.log("=========>", otp)
+        await setCache(genHash(email), otp, 120);
+    }
 };
 
 
