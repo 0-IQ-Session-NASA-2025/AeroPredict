0d2ad670b405de0ec31b4ccd8ec6c54e69bc3819
Arif Shahriar <arifshahriar028@gmail.com>
2025-10-03 19:00:16 +0600

    feat: add notification alert

--- backend-node/src/routes/v1/index.ts
+++ backend-node/src/routes/v1/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import otpRouter from './otp.routes';
 import publicRouter from './public.routes';
 import authRouter from './auth.routes';
+import notificationRouter from './notification.routes';
 
 const router = express.Router();
 
@@ -9,5 +10,6 @@ const router = express.Router();
 router.use('/otp', otpRouter);
 router.use('/public', publicRouter);
 router.use('/user/auth', authRouter);
+router.use('/fcm', notificationRouter);
 
 export default router;
